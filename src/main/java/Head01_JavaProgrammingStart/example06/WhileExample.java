52624e31d4a2622887eb08080520012ec229119b
jykim <jykim1428@hufs.ac.kr>
2025-08-08 15:11:19 +0900

    while

---
+++ src/main/java/Head01_JavaProgrammingStart/example06/WhileExample.java
@@ -0,0 +1,11 @@
+package Head01_JavaProgrammingStart.example06;
+
+public class WhileExample {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 10) {
+            System.out.println("count = " + count);
+            count++;
+        }
+    }
+}
